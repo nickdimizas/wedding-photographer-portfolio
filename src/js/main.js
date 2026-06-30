a5b78cc9680b4ca52a4cb67483e4c849b8a16404
Nick Dimizas <nikkosdim@gmail.com>
2026-06-30 13:10:30 +0300

    fix: typo on import lightbox.js in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import { initNavigation } from "./modules/navigation.js";
 import { initContactForm } from "./modules/contact-form.js";
-import { initLighbox } from "./modules/lightbox.js";
+import { initLightbox } from "./modules/lightbox.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initNavigation();
   initContactForm();
-  initLighbox();
+  initLightbox();
 });
